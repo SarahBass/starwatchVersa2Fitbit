ad408e3af197ad88513d9382d2466e9139c4b9b6
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-01-28 02:57:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ let date = document.getElementById("date");
 let month = document.getElementById("month");
 let day = document.getElementById("day");
 let year = document.getElementById("year");
+let hourhand = document.getElementById("hourhand");
+let minutehand = document.getElementById("minutehand");
+let minutehand2 = document.getElementById("minutehand2");
+let colon = document.getElementById("colon");
 // Update the clock every minute
 clock.granularity = "seconds";
 
@@ -61,8 +65,45 @@ clock.ontick = (evt) => {
   }
   let mins = util.zeroPad(today.getMinutes());
   myLabel.text = `${hours}:${mins}`;
+  if (hours == 1){hourhand.image = "hoursfile/hour1.png";}
+  else if (hours == 2){hourhand.image = "hoursfile/hour2.png";}
+  else if (hours == 3){hourhand.image = "hoursfile/hour3.png";}
+  else if (hours == 4){hourhand.image = "hoursfile/hour4.png";}
+  else if (hours == 5){hourhand.image = "hoursfile/hour5.png";}
+  else if (hours == 6){hourhand.image = "hoursfile/hour6.png";}
+  else if (hours == 7){hourhand.image = "hoursfile/hour7.png";}
+  else if (hours == 8){hourhand.image = "hoursfile/hour8.png";}
+  else if (hours == 9){hourhand.image = "hoursfile/hour9.png";}
+  else if (hours == 10){hourhand.image = "hoursfile/hour10.png";}
+  else if (hours == 11){hourhand.image = "hoursfile/hour11.png";}
+  else{hourhand.image = "hoursfile/hour12.png"}
   
-
+  if (mins%10 == 1 ){minutehand2.image = "minutesfile/1.png";}
+  else if (mins%10 == 2 ){minutehand2.image = "minutesfile/2.png";}
+  else if (mins%10 == 3 ){minutehand2.image = "minutesfile/3.png";}
+  else if (mins%10 == 4 ){minutehand2.image = "minutesfile/4.png";}
+  else if (mins%10 == 5 ){minutehand2.image = "minutesfile/5.png";}
+  else if (mins%10 == 6 ){minutehand2.image = "minutesfile/6.png";}
+  else if (mins%10 == 7 ){minutehand2.image = "minutesfile/7.png";}
+  else if (mins%10 == 8 ){minutehand2.image = "minutesfile/8.png";}
+  else if (mins%10 == 9 ){minutehand2.image = "minutesfile/9.png";}
+  else if (mins%10 == 0 ){minutehand2.image = "minutesfile/0.png";}
+  else{minutehand.image = "minutesfile/00.png";
+      minutehand2.image = " ";}
+  
+    if ( parseInt(mins/10) == 1 ){minutehand.image = "minutesfile/1.png";}
+  else if (parseInt(mins/10) == 2 ){minutehand.image = "minutesfile/2.png";}
+  else if ( parseInt(mins/10) == 3 ){minutehand.image = "minutesfile/3.png";}
+  else if (parseInt(mins/10) == 4 ){minutehand.image = "minutesfile/4.png";}
+  else if (parseInt(mins/10) == 5 ){minutehand.image = "minutesfile/5.png";}
+  else if (parseInt(mins/10) == 6 ){minutehand.image = "minutesfile/6.png";}
+  else if (parseInt(mins/10) == 7 ){minutehand.image = "minutesfile/7.png";}
+  else if (parseInt(mins/10) == 8 ){minutehand.image = "minutesfile/8.png";}
+  else if (parseInt(mins/10) == 9 ){minutehand.image = "minutesfile/9.png";}
+  else if (parseInt(mins/10) == 0 ){minutehand.image = "minutesfile/0.png";}
+  else{minutehand.image = "minutesfile/00.png";
+      minutehand2.image = " ";}
+  
    if (hours === 0 && mins === 0) {
   resetDate();}
   updateScene();
@@ -88,13 +129,37 @@ function checkAndUpdateBatteryLevel() {
 
   function updateScene() {
     date.text = dates;
-    month.text = months;
-    day.text = days;
+    printmonth();
+    printday();
     year.text = years;
   }
   
+function printday(){
+  if (days == 0){day.text = "Sunday,";}
+  else if (days == 1){day.text = "Monday,";}
+  else if (days == 2){day.text = "Tuesday,";}
+  else if (days == 3){day.text = "Wednesday,";}
+  else if (days == 4){day.text = "Thursday,";}
+  else if (days == 5){day.text = "Friday,";}
+  else if (days == 6){day.text = "Saturday,";}
+  else {day.text = "DAY";}
+}
 
-
+  function printmonth(){
+  if (months == 0){month.text = "January";}
+  else if (months == 1){month.text = "February";}
+  else if (months == 2){month.text = "March";}
+  else if (months == 3){month.text = "April";}
+  else if (months == 4){month.text = "May";}
+  else if (months == 5){month.text = "June";}
+  else if (months == 6){month.text = "July";}
+  else if (months == 7){month.text = "August";}
+  else if (months == 8){month.text = "Septemper";}
+  else if (months == 9){month.text = "October";}
+  else if (months == 10){month.text = "November";}
+  else if (months == 11){month.text = "December";}
+  else {month.text = "MONTH";}
+}
 
 
   
